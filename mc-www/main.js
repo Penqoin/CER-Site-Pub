44aefc7745491688b9963aa1298009317d786ebf
TheOriginalCER06 <69149072+TheOriginalCER06@users.noreply.github.com>
2024-06-08 18:42:39 +0200

    daily edits

---
+++ mc-www/main.js
@@ -0,0 +1,37 @@
+// Click-to-copy functionality
+document.querySelectorAll("code").forEach((codeBlock) => {
+    codeBlock.addEventListener("click", () => {
+        navigator.clipboard
+            .writeText(codeBlock.innerText)
+            .then(() => {
+                alert("Kopiert til utklippstavle!");
+            })
+            .catch((err) => {
+                console.error("Klarte ikke kopiere teksten: ", err);
+            });
+    });
+});
+const serverAddress = "play.cer06.no";
+
+            // Fetch server data from the MCSrvStat API
+            fetch(`https://api.mcsrvstat.us/3/${serverAddress}`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.online) {
+                        console.log("Server data:", data);
+                        document.getElementById("server").innerHTML = `
+                            <h2>Minecraft Info</h2>
+                            <div class="server-info">
+                            <p>${data.players.online} / ${data.players.max}</p>
+                            <p>${data.motd.html.join("<br>")}</p>
+                            <p>${data.version}</p>
+                            </div>
+                        `;
+                    } else {
+                        document.getElementById("server").innerHTML = "<p>Server is currently offline</p>";
+                    }
+                })
+                .catch(error => {
+                    console.error("Error fetching server data:", error);
+                    document.getElementById("server").innerHTML = "<p>Error fetching server data</p>";
+                });
